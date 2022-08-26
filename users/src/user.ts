aefeca7b39c4727cc16b1dcc38bbb3daec5533bc
Sabry Awad <dr.sabry1997@gmail.com>
2022-08-26 15:14:06 +0200

    Reading records from database

--- users/src/user.ts
+++ users/src/user.ts
@@ -1,4 +1,4 @@
-import { model, Schema, Document } from 'mongoose';
+import { model, Schema, Document, Model } from 'mongoose';
 
 interface IUser {
   name: string;
@@ -6,10 +6,19 @@ interface IUser {
 
 export interface IUserDoc extends IUser, Document {}
 
+interface IUserModel extends Model<IUser> {
+  any(...args: Parameters<typeof Model.findOne>): Promise<boolean>;
+}
+
 const UserSchema = new Schema<IUserDoc>({
   name: String,
 });
 
-const User = model('user', UserSchema);
+UserSchema.static('any', async function any(query) {
+  const result = await this.findOne(query).select('_id').lean();
+  return result ? true : false;
+});
+
+const User = model<IUserDoc, IUserModel>('user', UserSchema);
 
 export default User;
--- users/tests/user.test.ts
+++ users/tests/user.test.ts
@@ -8,6 +8,8 @@ describe('User', () => {
     _id: Types.ObjectId;
   };
 
+  const username = 'Joe';
+
   beforeAll(async () => {
     await database.connect('users_test');
   });
@@ -22,7 +24,7 @@ describe('User', () => {
     });
 
     it('should save a new user', async () => {
-      user = new User({ name: 'Joe' });
+      user = new User({ name: username });
       await user.save();
       expect(!user.isNew).toEqual(true);
     });
@@ -30,7 +32,7 @@ describe('User', () => {
 
   describe('Reading records', () => {
     it('should find records with a given name', async () => {
-      const users = await User.find({ name: 'Joe' }).select('name');
+      const users = await User.find({ name: username }).select('name');
       expect(users[0]._id).toEqual(user._id);
       expect(users[0]._id.toString()).toBe(user._id.toString());
     });
@@ -38,6 +40,42 @@ describe('User', () => {
     it('should find record with a given id', async () => {
       const found = await User.findOne({ id: user._id });
       expect(found!.name).toEqual(user.name);
+    });
+  });
+
+  describe('Deleting records', () => {
+    beforeEach(async () => {
+      await User.collection.drop();
+      user = new User({ name: username });
+      await user.save();
+    });
+
+    it('should remove record from the database using model instance method **remove**', async () => {
+      await user.remove();
+      const count = await User.countDocuments({ _id: user._id });
+      expect(count).toBe(0);
+    });
+
+    it('should remove record from the database using class method **remove**', async () => {
+      await User.remove({ _id: user._id });
+      const doesUserExit = await User.findOne({ _id: user._id })
+        .select('_id')
+        .lean(/* the documents are returned as plain objects */)
+        .then(doc => !!doc);
+
+      expect(doesUserExit).toBeFalsy();
+    });
+
+    it('should remove record from the database using class method **findOneAndRemove**', async () => {
+      await User.findOneAndRemove({ _id: user._id });
+      const doesUserExit = await User.exists({ _id: user._id });
+      expect(doesUserExit).toBeNull();
+    });
+
+    it('should remove record from the database using class method **findByIdAndRemove**', async () => {
+      await User.findByIdAndRemove({ _id: user._id });
+      const doesUserExit = await User.any({ _id: user._id });
+      expect(doesUserExit).toBeFalsy();
     });
   });
 });
